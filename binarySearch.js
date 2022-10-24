edee385461db40dca8d19471692b53d60bc48d3c
Vicki Wong <vicki.wong85@gmail.com>
2022-10-24 14:50:30 -0500

    completed binary search and minSubArrayLen

---
+++ binarySearch.js
@@ -0,0 +1,35 @@
+// debugger;
+function binarySearch(array, num){
+  var left = 0;
+  var right = array.length - 1;
+  while (left < right ) {
+      var mid = Math.ceil((left + right) / 2);
+      if (array[mid] === num) {
+          return mid;
+      }
+      if (array[mid] < num) {
+          left = mid;
+      }
+      if (array[mid] > num) {
+          right = mid;
+      }
+  }
+  return -1;
+}
+
+console.log(binarySearch([1,2,3,4,5],2)) // 1
+console.log(binarySearch([1,2,3,4,5],3)) // 2
+console.log(binarySearch([1,2,3,4,5],5)) // 4
+console.log(binarySearch([1,2,3,4,5],6)) // -1
+console.log(binarySearch([
+  5, 6, 10, 13, 14, 18, 30, 34, 35, 37,
+  40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+], 10)) // 2
+console.log(binarySearch([
+  5, 6, 10, 13, 14, 18, 30, 34, 35, 37,
+  40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+], 95)) // 16
+console.log(binarySearch([
+  5, 6, 10, 13, 14, 18, 30, 34, 35, 37,
+  40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+], 100)) // -1
